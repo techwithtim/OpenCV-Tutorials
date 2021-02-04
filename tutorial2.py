dbc616c398ae11ae7222faeeb85320a9b8cedab7
Tim <mrtechguy234@gmail.com>
2021-02-03 22:17:23 -0500

    added tutorial 2

---
+++ tutorial2.py
@@ -0,0 +1,17 @@
+import cv2
+import random
+
+img = cv2.imread('assets/logo.jpg', -1)
+
+# Change first 100 rows to random pixels
+for i in range(100):
+	for j in range(img.shape[1]):
+		img[i][j] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+
+# Copy part of image
+tag = img[500:700, 600:900]
+img[100:300, 650:950] = tag
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

c44059c076cfdbb12e3af1b49c2819f47bb29959
Tim <mrtechguy234@gmail.com>
2021-02-04 21:51:31 -0500

    added tutorial 3

---
+++ tutorial3.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    image = np.zeros(frame.shape, np.uint8)
+    smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
+    image[height//2:, :width//2] = smaller_frame
+    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
+    image[height//2:, width//2:] = smaller_frame
+
+    cv2.imshow('frame', image)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()

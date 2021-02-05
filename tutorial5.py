d452ce0a9b9275da01ab9bfcdc2351b142c96a39
Tim <mrtechguy234@gmail.com>
2021-02-04 23:20:00 -0500

    added tutorial 5

---
+++ tutorial5.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([90, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('frame', result)
+    cv2.imshow('mask', mask)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()

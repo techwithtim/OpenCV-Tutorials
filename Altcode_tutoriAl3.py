888f1e00a11bf08102cb86b54971a19848eca976
veeru09042004 <50945127+veeru09042004@users.noreply.github.com>
2021-03-02 00:33:04 +0530

    Create Altcode_tutoriAl3.py
    Hey Tim, or TWT team member who manages this repo, the above python file is an alternate code for the program done in tutorial 3, the one to make live video feed appear 4 times in the same window. I did not know how to reach out to you, so i used github. The important lines you need to look at is from 12 to 26. This code basically shows you *dont need cv2.rotate()* method. BTW plsss continue the opencv series. It's really well executed! This is my first time using github.....so not sure what i'm really doing or whether this file would reach you or not :/

---
+++ Altcode_tutoriAl3.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+cap=cv2.VideoCapture(0)
+
+while True:
+    ret,frame=cap.read()#ret returns True or False, True if video capture is complete
+    width,height=int(cap.get(3)),int(cap.get(4))
+
+    image=np.zeros(frame.shape, np.uint8)
+    frameshrunk=cv2.resize(frame,(0,0),fx=0.5,fy=0.5)#Shrinks to 1/4 th as fx*fy=0.25
+    image[:height//2, :width//2]=frameshrunk [:,::-1]          #first quadrant, similar cartesian i.e. top right is 1st quadrant; frameshrunk[keeps it straight:flips horizontally ie across y axis]
+    image[:height//2, width//2:] = frameshrunk                   #2nd quadrnt, top left is 2nd quadrant
+    image[height//2:, width//2:] = frameshrunk[::-1,:]         # 3rd quadrant, bottom left is 3rd quadrant
+    image[height//2:, :width//2:] = frameshrunk[::-1,::-1]    # 4th quadrant, bottom right is 4th quadrant
+
+#Reference graph below:
+#               |
+# IInd quadrant |  1st quadrant
+#               |
+#               |
+#  _____________|_______________
+#               |
+#               |
+#     3rd       |      4th
+#               |
+ 
+                       
+    cv2.imshow('frame',image)
+
+    if cv2.waitKey(1)==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
